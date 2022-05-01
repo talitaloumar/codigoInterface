a7f69a157bd47bfb84cf4a909aafde0f5242ac4b
talitaloumar <103341433+talitaloumar@users.noreply.github.com>
2022-05-01 19:44:37 -0300

    INTEGRAÇÃO COM PROJETO ACADEMIA

---
+++ src/Gym/Company/ProjetoAcademia.java
@@ -0,0 +1,33 @@
+package Gym.Company;
+
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+
+public class ProjetoAcademia {
+    private JPanel panel1;
+    private JFormattedTextField formattedTextField1;
+    private JButton ENVIAR;
+    private JCheckBox alunoACheckBox;
+    private JCheckBox administraçãoCheckBox;
+    private JCheckBox professorACheckBox;
+    private JFormattedTextField c_id;
+    private JFormattedTextField c_endereco;
+    private JFormattedTextField c_tel;
+    private JFormattedTextField c_email;
+    private JFormattedTextField c_nasc;
+    private JFormattedTextField c_ultimoNome;
+    private JFormattedTextField c_sobreNome;
+    private JFormattedTextField c_nome;
+
+
+    public ProjetoAcademia() {
+        panel1.addComponentListener(new ComponentAdapter() {
+        });
+    }
+
+    private void createUIComponents() {
+
+
+
+    }
+}
